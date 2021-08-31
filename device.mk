f840dd2f032d09d4b85c707107f5ddc556c7f3d9
EngMoPro <fightstart20000@yahoo.com>
2021-08-31 10:34:48 -0400

    add more stable hal for display

--- device.mk
+++ device.mk
@@ -419,19 +419,17 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.config@1.8 \
-    libvulkan \
+    copybit.msm8937 \
     gralloc.msm8937 \
     hwcomposer.msm8937 \
     memtrack.msm8937 \
     libdisplayconfig \
     libqdMetaData.system \
-    libqdMetaData.system \
+    libgenlock \
     libtinyxml
-
 
 
 # Screen density

a286cf7a62c760daf5637716f7c54b68ef9eabc3
EngMoPro <fightstart20000@yahoo.com>
2021-09-25 13:30:37 +0200

    use eng defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -229,7 +229,7 @@ TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/olive
-TARGET_KERNEL_CONFIG := olive-perf_defconfig
+TARGET_KERNEL_CONFIG := eng_olive_defconfig
 #TARGET_KERNEL_CLANG_COMPILE := true
 #TARGET_PREBUILT_KERNEL:= device/xiaomi/olive/kernel
 
